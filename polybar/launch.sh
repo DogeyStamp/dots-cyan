d657ca03255f60d37f02aaf3c6980b618a7aa02e
DogeyStamp <adeng.sean@gmail.com>
2020-10-31 12:25:14 -0400

    Prevent double polybar

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -11,7 +11,10 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 #sleep 3
 
 # Launch bar1 and bar2
+
 polybar top-main -c ~/.config/polybar/config-top.ini &
 polybar bottom-main -c ~/.config/polybar/config-bottom.ini &
-polybar bottom-ext -c ~/.config/polybar/config-bottom.ini &
-polybar top-ext -c ~/.config/polybar/config-top.ini &
+if [[ $(xrandr | grep "DP1 connected" | grep -Eiv "eDP1") ]]; then
+    polybar bottom-ext -c ~/.config/polybar/config-bottom.ini &
+    polybar top-ext -c ~/.config/polybar/config-top.ini &
+fi
